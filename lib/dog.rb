a04ed832c58971b727b46c078efc6f9552e7a6e0
Rayyan A. Albaz <ra5analbaz@hotmail.com>
2018-04-30 18:39:36 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def initialize(name , breed = "Mutt")
+    @breed = breed
+    @name = name
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+  def breed
+    @bred
+  end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+end
